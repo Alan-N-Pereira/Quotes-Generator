30b609223fb0ab29a98899c6d12f696221442262
Alan-N-Pereira <itsalanpereira@gmail.com>
2021-08-26 17:22:15 +0530

    Quotes Generator Completed

---
+++ Readme.md
@@ -0,0 +1,23 @@
+# Quotes Generator
+
+This quote generator website is built using "**HTML**" , "**CSS**" and "**JAVASCRIPT**".
+
+And all the quotes on the site are **Dynamically** generated using **Asynchronous Fetch()** request which fetches a random quote from a "*Rest Quotes API*".
+
+The Site contains Two buttons 
+1. A "*New Quote Button*" which when pressed Randomly selects a new quotes from the fetched data from the api and populates the DOM.
+2. A "*Twitter Button*" which takes you to twitter on new tab, that if when Logged In allows you to tweet the quote which was generated.
+
+<p>&nbsp<p>
+
+## Screenshot of the Site
+<p>&nbsp<p>
+
+1. **Normal View**
+<p>&nbsp<p>
+<img src="Images/site-screenshot.png" width="700px">
+<p>&nbsp<p>
+
+2. **Responsive View**
+<p>&nbsp<p>
+<img src="Images/site-screenshot-res.png" width="700px">
--- script.js
+++ script.js
@@ -1,1 +1,71 @@
-console.log('Test');
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+let apiQuotes = [];
+
+// Show Loading
+function loading() {
+	loader.hidden = false;
+	quoteContainer.hidden = true;
+}
+
+function complete() {
+	quoteContainer.hidden = false;
+	loader.hidden = true;
+}
+
+// Show New Quote
+function newQuote() {
+	loading();
+	// Pick a random quote from apiQuotes
+	const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+	// Check Quote length to determine styling
+	if (quote.text.length > 120) {
+		quoteText.classList.add('long-quote');
+	} else {
+		quoteText.classList.remove('long-quote');
+	}
+	quoteText.textContent = quote.text;
+
+	// Check if Author field is blank replace it with "Unknown"
+	if (quote.author == null) {
+		authorText.textContent = '-Unknown';
+	} else {
+		authorText.textContent = '-' + quote.author;
+	}
+
+	// Complete loading
+	complete();
+}
+
+// Get Quotes From API
+async function getQuotes() {
+	loading();
+	const apiUrl = 'https://type.fit/api/quotes';
+	try {
+		const response = await fetch(apiUrl);
+		apiQuotes = await response.json();
+		// console.log(apiQuotes);
+		newQuote();
+	} catch (error) {
+		// Catch Error
+	}
+}
+
+// Tweet A Quote
+function tweetQuote() {
+	const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+	window.open(twitterUrl, '_blank');
+}
+
+// Event Listener
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// On load
+getQuotes();
